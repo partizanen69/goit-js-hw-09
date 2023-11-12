a13a409635884e26719e5d448a4bd1e424d1c437
Oleksii Ablitsov <oleksii.ablitsov@bringg.com>
2023-11-12 13:36:50 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const body = document.querySelector('body');
+let startBtn = null;
+let stopBtn = null;
+let timer = null;
 
+window.onload = () => {
+  startBtn = document.querySelector('button[data-start]');
+  stopBtn = document.querySelector('button[data-stop]');
+
+  console.log({ startBtn, stopBtn });
+
+  startBtn.addEventListener('click', startOnClick);
+  stopBtn.addEventListener('click', stopOnClick);
+};
+
+const startOnClick = e => {
+  startBtn.disabled = true;
+  timer = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+};
+
+const stopOnClick = e => {
+  clearInterval(timer);
+  startBtn.disabled = false;
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
